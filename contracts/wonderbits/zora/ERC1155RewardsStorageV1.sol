60a101d21030a2b273fa3896245eed3a18edaad1
suwandre <suwandresukijat@gmail.com>
2024-06-26 11:15:16 +0200

    rf: removed zora-related contracts

--- contracts/wonderbits/zora/ERC1155RewardsStorageV1.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
-
-contract ERC1155RewardsStorageV1 {
-    mapping(uint256 => address) public createReferrals;
-
-    mapping(uint256 => address) public firstMinters;
-}
--- contracts/wonderbits/zora/IERC7572.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
-
-interface IERC7572 {
-    function contractURI() external view returns (string memory);
-
-    event ContractURIUpdated();
-}
