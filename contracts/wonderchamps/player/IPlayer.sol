c15aa5360de4661666a0c64a776310fde1618378
suwandre <suwandresukijat@gmail.com>
2024-06-27 12:23:50 +0200

    rf: removed unused interfaces after refactoring

--- contracts/wonderchamps/player/IPlayer.sol
+++ contracts/wonderchamps/player/IPlayer.sol
@@ -2,26 +2,15 @@
 
 pragma solidity ^0.8.26;
 
-import "../inventory/IInventory.sol";
-import "../stats/IInGameStats.sol";
+// import "../inventory/IInventory.sol";
+// import "../stats/IInGameStats.sol";
+
+import "../items/IItem.sol";
+import "../items/IItemFragment.sol";
+import "../stats/ILeagueData.sol";
 
 // Interface for a player's data.
-interface IPlayer is IInventory, IInGameStats {
-    // represents a player's data instance.
-    struct Player {
-        // the user's address
-        address addr;
-        // the user's owned in-game currencies (IGC)
-        // BIT POSITIONS:
-        // [0 - 127] - gold (premium currency)
-        // [128 - 255] - marble (common currency)
-        uint256 ownedIGC;
-        // the user's inventory
-        Inventory inventory;
-        // the user's in-game stats
-        InGameStats inGameStats;
-    }
-
+interface IPlayer is IItem, IItemFragment, ILeagueData {
     function getPlayer(address player) external view returns (uint256, OwnedItem[] memory, OwnedItemFragment[] memory, uint256, LeagueData[] memory);
     function playerExists(address player) external view returns (bool);
     function createPlayer(address player, bytes32 salt, uint256 timestamp, bytes calldata adminSig) external;
